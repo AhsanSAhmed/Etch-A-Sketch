d6c421d61fa03e07302ee36610a4f4afcf628922
AhsanSAhmed <ahsanahmedshajee@gmail.com>
2024-05-05 00:47:38 +0530

    Added a resizable grid according to user input and some styling

--- etch-a-sketch/javascript.js
+++ etch-a-sketch/javascript.js
@@ -1,16 +1,44 @@
 const container=document.querySelector(".container");
 const globalHeight=400,globalWidth=400;
+let gridRows=16,gridColumns=16;
+
 
 let mouseDown = false
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
 
-function createGrid()
+const form=document.querySelector("#gridSize");
+form.addEventListener("submit", (e)=>{e.preventDefault();
+    const gridRowsInput = document.querySelector("#gridRows");
+    const gridColumnsInput = document.querySelector("#gridColumns");
+    const newgridRows=parseInt(gridRowsInput.value);
+    const newgridColumns=parseInt(gridColumnsInput.value);
+
+    if (!isNaN(newgridRows) && !isNaN(newgridColumns)) 
+    {
+        gridRows = newgridRows;
+        gridColumns = newgridColumns;
+        createGrid(gridRows, gridColumns);
+    }
+    else
+    {
+        createGrid(16, 16);
+    }
+    });
+
+function createGrid(gridRows,gridColumns)
 {
-    for(let i=0;i<(16 ** 2);i++)
+    let boxHeight=globalHeight / gridRows;
+    let boxWidth=globalHeight / gridColumns;
+
+    container.innerHTML = "";
+
+    for(let i=0;i<(gridRows*gridColumns);i++)
     {
         const box=document.createElement("div");
-        box.setAttribute("style", "width:25px;height:25px;outline:black solid 1px;");
+        box.style.width = boxWidth + "px";
+        box.style.height = boxHeight + "px";
+        box.style.outline = "1px solid black";
         box.addEventListener('mousedown', changeColor);
         box.addEventListener('mouseover',changeColor);
         container.appendChild(box);
@@ -20,7 +48,7 @@ function createGrid()
 function changeColor(e)
 {
     if(e.type==='mouseover' && !mouseDown) return;
-    e.target.style.backgroundColor = "blue";
+    e.target.style.backgroundColor = "#333333";
 }
 
-createGrid();
+createGrid(gridRows,gridColumns);
